57ee00a9eda274bce634cf49b86ec4e38852d87c
Manuel Lamas <moncholamas@gmail.com>
2021-09-17 14:28:02 -0300

    Base de datos conectada y configurado Mongoose

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import 'dotenv/config';
 import app from './app';
+import mongoose from 'mongoose';
+
 
 
 const main = () =>{
+    mongoose.connect('mongodb://localhost:27017/pushka',()=>{
+        console.log('Conectado a la DB'); 
+    });
+
     app.listen(process.env.PORT,()=>{
         console.log('servidor arrancado');
     });
